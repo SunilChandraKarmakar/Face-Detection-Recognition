c62be931f3a5f8d35f7c6831572b9d2dab5b96ff
Sunil Chandra Karmakar <sunil_karmakar@ymail.com>
2019-06-29 16:12:51 +0600

    Update Design

--- FaceDetectionRecognition/FaceDetectionRecognition/Form1.Designer.cs
+++ FaceDetectionRecognition/FaceDetectionRecognition/Form1.Designer.cs
@@ -33,20 +33,27 @@
             this.detectionRecognitionBtn = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.lll = new System.Windows.Forms.Label();
+            this.txtCrime = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.cameraBox)).BeginInit();
             this.SuspendLayout();
             // 
             // cameraBox
             // 
+            this.cameraBox.BackColor = System.Drawing.SystemColors.ButtonShadow;
             this.cameraBox.Location = new System.Drawing.Point(12, 12);
             this.cameraBox.Name = "cameraBox";
-            this.cameraBox.Size = new System.Drawing.Size(505, 383);
+            this.cameraBox.Size = new System.Drawing.Size(498, 383);
             this.cameraBox.TabIndex = 2;
             this.cameraBox.TabStop = false;
             // 
             // detectionRecognitionBtn
             // 
-            this.detectionRecognitionBtn.Location = new System.Drawing.Point(114, 420);
+            this.detectionRecognitionBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.detectionRecognitionBtn.Location = new System.Drawing.Point(114, 412);
             this.detectionRecognitionBtn.Name = "detectionRecognitionBtn";
             this.detectionRecognitionBtn.Size = new System.Drawing.Size(247, 35);
             this.detectionRecognitionBtn.TabIndex = 3;
@@ -58,7 +65,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(542, 103);
+            this.label1.Location = new System.Drawing.Point(536, 112);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(45, 16);
             this.label1.TabIndex = 4;
@@ -67,22 +74,75 @@
             // txtName
             // 
             this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtName.Location = new System.Drawing.Point(593, 100);
+            this.txtName.Location = new System.Drawing.Point(587, 109);
             this.txtName.Name = "txtName";
             this.txtName.Size = new System.Drawing.Size(247, 22);
             this.txtName.TabIndex = 5;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAge.Location = new System.Drawing.Point(587, 142);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(247, 22);
+            this.txtAge.TabIndex = 7;
+            // 
+            // lll
+            // 
+            this.lll.AutoSize = true;
+            this.lll.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lll.Location = new System.Drawing.Point(536, 145);
+            this.lll.Name = "lll";
+            this.lll.Size = new System.Drawing.Size(33, 16);
+            this.lll.TabIndex = 6;
+            this.lll.Text = "Age";
+            // 
+            // txtCrime
+            // 
+            this.txtCrime.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCrime.Location = new System.Drawing.Point(587, 176);
+            this.txtCrime.Multiline = true;
+            this.txtCrime.Name = "txtCrime";
+            this.txtCrime.Size = new System.Drawing.Size(247, 81);
+            this.txtCrime.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(536, 208);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Crime";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(626, 270);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(166, 29);
+            this.btnSave.TabIndex = 10;
+            this.btnSave.Text = "Save Information";
+            this.btnSave.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(868, 467);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtCrime);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.lll);
             this.Controls.Add(this.txtName);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.detectionRecognitionBtn);
             this.Controls.Add(this.cameraBox);
             this.Name = "Form1";
             this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Face Detection Recognition";
             ((System.ComponentModel.ISupportInitialize)(this.cameraBox)).EndInit();
             this.ResumeLayout(false);
@@ -96,6 +156,11 @@
         private System.Windows.Forms.Button detectionRecognitionBtn;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.Label lll;
+        private System.Windows.Forms.TextBox txtCrime;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSave;
     }
 }
 
