ac983926af7d305521d2bc596b402e5fe1c0ca99
Sunil Chandra Karmakar <sunil_karmakar@ymail.com>
2019-06-29 14:57:25 +0600

    Add Faces store folder and write code for store faces

--- FaceDetectionRecognition/FaceDetectionRecognition/Form1.cs
+++ FaceDetectionRecognition/FaceDetectionRecognition/Form1.cs
@@ -1,9 +1,11 @@
 ﻿using Emgu.CV;
+using Emgu.CV.Structure;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,11 +17,32 @@ namespace FaceDetectionRecognition
     {
         //Declear variable
         HaarCascade faceDetected;
+        Image<Bgr, byte> frame;
+        Capture camera;
+        Image<Gray, byte> result;
+        Image<Gray, byte> trainedFace = null;
+        Image<Gray, byte> grayFace = null;
+        List<Image<Gray, byte>> trainingImages = new List<Image<Gray, byte>>();
+        List<string> labels = new List<string>();
+        List<string> user = new List<string>();
+        int count, numLabels, t;
+        string name, names = null;
 
         public Form1()
         {
             InitializeComponent();
+
+            //HaarCascade is for face detection
             faceDetected = new HaarCascade("haarcascade_frontalface_default.xml");
+
+            try
+            {
+                string labelsInfo = File.ReadAllText(Application.StartupPath + "/Faces/Faces.txt");
+            }
+            catch
+            {
+                MessageBox.Show("Nothing in show Database");
+            }
         }
 
         private void detectionRecognitionBtn_Click(object sender, EventArgs e)
